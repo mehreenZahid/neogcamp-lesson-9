546403cbf1dcf605032b9da2796a55c6d428014e
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-14 22:51:00 +0530

    till ex-05 done

--- script.js
+++ script.js
@@ -14,6 +14,60 @@ function isPalindrome(str){
     return false;
 }
 
-console.log(isPalindrome('404'));
-console.log(isPalindrome('civic'));
-console.log(isPalindrome('momo'))
+function convertDateToStr(date){
+    var dateStr = { day: '', month: '', year: ''}
+
+    if(date.day < 10){
+        dateStr.day = '0' + date.day 
+    }
+    else{
+        dateStr.day = date.day.toString()
+    }
+
+    if(date.month < 10){
+        dateStr.month = '0' + date.month 
+    }
+    else{
+        dateStr.month = date.month.toString()
+    }
+
+    dateStr.year = date.year.toString()
+
+    return dateStr
+}
+
+function getAllDateFormats(date){
+    var dateStr = convertDateToStr(date)
+
+    var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year
+    var mmddyyyy = dateStr.month + dateStr.day + dateStr.year
+    var yyyymmdd = dateStr.year + dateStr.month + dateStr.day
+    var ddmmyy = dateStr.day + dateStr.month + dateStr.year.slice(-2)
+    var mmddyy = dateStr.month + dateStr.day + dateStr.year.slice(-2)
+    var yymmdd = dateStr.year.slice(-2) + dateStr.month + dateStr.day
+
+    return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd]
+}
+
+function checkPalindromeForAllDateFormats(date){
+    var listOfPalindromes = getAllDateFormats(date)
+    
+    var checkIsPalindrome = false
+
+    for(var i=0; i < listOfPalindromes.length; i++){
+        if(isPalindrome(listOfPalindromes[i])){
+            checkIsPalindrome = true;
+            break;
+        }        
+    }
+
+    return checkIsPalindrome
+}
+
+var date = {
+    day: 2,
+    month: 11,
+    year: 2020
+}
+
+console.log(checkPalindromeForAllDateFormats(date))
