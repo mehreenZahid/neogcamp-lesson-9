5b7faea26299d9d39c928484f37c4088493a42fd
mehreenzahid <93670902+mehreenZahid@users.noreply.github.com>
2023-11-16 20:44:43 +0530

    js part done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function reverseStr(str){
+function reverseStr(str) {
     var listOfChars = str.split('');
     var reverseListOfChars = listOfChars.reverse();
     var reverseStr = reverseListOfChars.join('');
@@ -6,28 +6,28 @@ function reverseStr(str){
     return reverseStr;
 }
 
-function isPalindrome(str){
+function isPalindrome(str) {
     var reverse = reverseStr(str);
-    if(str === reverse){
+    if (str === reverse) {
         return true;
     }
     return false;
 }
 
-function convertDateToStr(date){
-    var dateStr = { day: '', month: '', year: ''}
+function convertDateToStr(date) {
+    var dateStr = { day: '', month: '', year: '' }
 
-    if(date.day < 10){
-        dateStr.day = '0' + date.day 
+    if (date.day < 10) {
+        dateStr.day = '0' + date.day
     }
-    else{
+    else {
         dateStr.day = date.day.toString()
     }
 
-    if(date.month < 10){
-        dateStr.month = '0' + date.month 
+    if (date.month < 10) {
+        dateStr.month = '0' + date.month
     }
-    else{
+    else {
         dateStr.month = date.month.toString()
     }
 
@@ -36,7 +36,7 @@ function convertDateToStr(date){
     return dateStr
 }
 
-function getAllDateFormats(date){
+function getAllDateFormats(date) {
     var dateStr = convertDateToStr(date)
 
     var ddmmyyyy = dateStr.day + dateStr.month + dateStr.year
@@ -49,25 +49,95 @@ function getAllDateFormats(date){
     return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd]
 }
 
-function checkPalindromeForAllDateFormats(date){
+function isLeapYear(year) {
+    if (year % 400 === 0) {
+        return true;
+    }
+    if (year % 100 === 0) {
+        return false;
+    }
+    if (year % 4 === 0) {
+        return true
+    }
+    return false
+}
+
+function getNextDate(date) {
+    var day = date.day + 1
+    var month = date.month
+    var year = date.year
+
+    var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if (month === 2) {
+        if (isLeapYear(year)) {
+            if (day > 29) {
+                day = 1
+                month++
+            }
+        }
+        else {
+            if (day > 28) {
+                day = 1
+                month++
+            }
+        }
+    }
+    else {
+        if (day > daysInMonth[month - 1]) {
+            day = 1
+            month++
+        }
+    }
+
+    if (month > 12) {
+        month = 1
+        year++
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year
+    }
+}
+
+function getNextPalindromeDate(date){
+    var ctr = 0
+    var nextDate = getNextDate(date)
+
+    while(1){
+        ctr++
+        var isPalindrome = checkPalindromeForAllDateFormats(nextDate)
+
+        if(isPalindrome){
+            break;
+        }
+        nextDate = getNextDate(nextDate)
+    }
+
+    return [ctr, nextDate]
+}
+
+/* function getPreviousDate(date){
+    var day = date.day - 1
+    var month = date.month
+    var year = date.year
+
+
+} */
+
+function checkPalindromeForAllDateFormats(date) {
     var listOfPalindromes = getAllDateFormats(date)
-    
+
     var checkIsPalindrome = false
 
-    for(var i=0; i < listOfPalindromes.length; i++){
-        if(isPalindrome(listOfPalindromes[i])){
+    for (var i = 0; i < listOfPalindromes.length; i++) {
+        if (isPalindrome(listOfPalindromes[i])) {
             checkIsPalindrome = true;
             break;
-        }        
+        }
     }
 
     return checkIsPalindrome
 }
-
-var date = {
-    day: 2,
-    month: 11,
-    year: 2020
-}
-
-console.log(checkPalindromeForAllDateFormats(date))
